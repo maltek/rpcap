454107a34b4c706a46595126180795ef76d6de99
Malte Kraus <git@maltekraus.de>
2017-08-01 18:50:35 +0200

    fix tests: do not use random PCAP from my HDD

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,7 @@ use std::io;
 
 
 /// The `CapturedPacket` struct contains information about a single captured packet.
+#[derive(Eq,PartialEq,Debug)]
 pub struct CapturedPacket<'a> {
     /// The time when the packet was captured.
     pub time: time::Timespec,
@@ -89,5 +90,76 @@ impl error::Error for PcapError {
             PcapError::Io(ref err) => Some(err),
             _ => None,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::io::Write;
+
+    use super::write::{PcapWriter, WriteOptions};
+    use super::read::PcapReader;
+    use super::{CapturedPacket, Linktype};
+
+    use super::time::Timespec;
+
+    extern crate rand;
+    use self::rand::Rng;
+
+    /// Generates 10 buffers with random data.
+    fn gen_packet_data() -> Vec<Vec<u8>> {
+        let mut rng = rand::thread_rng();
+        (0..10).map(|_| {
+            let size = rng.gen_range::<usize>(0, 2000);
+            rng.gen_iter::<u8>().take(size).collect()
+        }).collect()
+    }
+
+    /// Generates a random packet for every buffer in `contents`.
+    fn gen_packets<'a>(contents: &'a Vec<Vec<u8>>, snaplen: usize) -> Vec<CapturedPacket<'a>> {
+        let mut rng = rand::thread_rng();
+
+        contents.iter().map(|data| {
+            let s = rng.gen_range::<i64>(0, i64::from(u32::max_value()) + 1);
+            let ns = rng.gen_range::<i32>(0, 1_000_000_000);
+
+            CapturedPacket {
+                time: Timespec::new(s, ns),
+                data: &data.chunks(snaplen).next().unwrap(),
+                orig_len: data.len(),
+            }
+        }).collect()
+    }
+
+    /// Writes `packets` to the `writer`. Returns the underlying writer of `writer`.
+    fn write_packets<W: Write>(mut writer: PcapWriter<W>, packets: &[CapturedPacket]) -> W {
+        for p in &packets[..5] {
+            writer.write(&p).unwrap();
+        }
+        writer.take_writer()
+    }
+
+    #[test]
+    fn read_write() {
+        let contents = gen_packet_data();
+        let packets = gen_packets(&contents, 1000);
+
+        let opts = WriteOptions {
+            snaplen: 1000,
+            linktype: Linktype::NULL.into(),
+        };
+
+        let mut buf = write_packets(PcapWriter::new(Vec::new(), opts).unwrap(), &packets[..5]);
+        buf = write_packets(PcapWriter::append(buf, opts).unwrap(), &packets[5..]);
+
+        let mut reader = PcapReader::new(buf.as_slice()).unwrap();
+        assert_eq!(reader.get_linktype(), Linktype::NULL.into());
+        assert_eq!(reader.get_snaplen(), 1000);
+        let mut i = 0;
+        while let Some(packet) = reader.next().unwrap() {
+            assert_eq!(packet, packets[i]);
+            i += 1;
+        }
+        assert_eq!(i, packets.len());
     }
 }
